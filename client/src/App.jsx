a32f18d47c0faa5df21342f06a8d58d43f7045c8
Muchammad In'am Awaludin <inam.awaludin@gmail.com>
2023-08-21 13:17:21 +0700

    Setup Index js on client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,27 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import HomeLayout from "./pages/HomeLayout";
-
+import {
+  HomeLayout,
+  Register,
+  Login,
+  DashboardLayout,
+  Error
+} from './pages'
 const router = createBrowserRouter([
   {
     path : '/',
     element : <HomeLayout />
   },
   {
-    path : '/about',
-    element : <div>
-      <h1>About Page</h1>
-    </div>
+    path : '/register',
+    element : <Register />
+  },
+  {
+    path : '/login',
+    element : <Login />
+  },
+  {
+    path : '/dashboard',
+    element : <DashboardLayout />
   }
 ]);
 
