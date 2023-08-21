e0a950ed24ff9611d8ef7eba6d7f3db1ef6eeaa0
Muchammad In'am Awaludin <inam.awaludin@gmail.com>
2023-08-21 12:42:33 +0700

    Initiate React project

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import HomeLayout from "./pages/HomeLayout";
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <HomeLayout />
+  },
+  {
+    path : '/about',
+    element : <div>
+      <h1>About Page</h1>
+    </div>
+  }
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />
+}
+
+export default App
---
+++ client/src/pages/DeleteJob.jsx
@@ -0,0 +1,6 @@
+const DeleteJob = () => {
+  return (
+    <div>DeleteJob Page</div>
+  )
+}
+export default DeleteJob
