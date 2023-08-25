5875eaa181c5f84c0aea3c9032368d1eb3a338a7
Muchammad In'am Awaludin <inam.awaludin@gmail.com>
2023-08-25 15:10:46 +0700

    Setup login and register page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,14 +1,28 @@
 import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import img from '../assets/images/not-found.svg';
+
+
 
 const Error = () => {
   const error = useRouteError();
 
-  console.log(error);
+  if (error.status === 404) {
+    return <Wrapper>
+      <div>
+        <img src={img} alt="Not Found" />
+        <h3>Page not found</h3>
+        <p>we can't seem to find the page you looking for</p>
+        <Link to='/dashboard' >Back home</Link>
+      </div>
+    </Wrapper>
+  }
   return (
-    <div>
-      <h1>Error Page</h1>
-      <Link to="/">Back Home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong!</h3>
+      </div>
+    </Wrapper>
   );
 };
 export default Error;
