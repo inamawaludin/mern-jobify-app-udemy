c43f7c65e46735bd66d37ac0fbc649d09afece70
Muchammad In'am Awaludin <inam.awaludin@gmail.com>
2023-08-26 20:08:19 +0700

    implements small and big side bar to navbar

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,6 +1,59 @@
+import { Outlet } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Dashboard";
+import { BigSidebar, Navbar, SmallSidebar } from "../components";
+import { createContext, useContext, useState } from "react";
+import { checkDefaultTheme } from "../App";
+
+const DashboardContext = createContext();
+
 const DashboardLayout = () => {
+  // temp
+  const user = { name: "udin" };
+  const [showSidebar, setShowsidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme());
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+
+    document.body.classList.toggle("dark-theme", newDarkTheme);
+    localStorage.setItem("darkTheme", newDarkTheme);
+  };
+
+  const toggleSidebar = () => {
+    setShowsidebar(!showSidebar);
+  };
+
+  const logoutUser = async () => {
+    console.log("logout user");
+  };
+
   return (
-    <h1>DashboardLayout Page</h1>
-  )
-}
-export default DashboardLayout
+    <DashboardContext.Provider
+      value={{
+        user,
+        showSidebar,
+        isDarkTheme,
+        toggleDarkTheme,
+        toggleSidebar,
+        logoutUser,
+      }}
+    >
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </DashboardContext.Provider>
+  );
+};
+
+export const useDashboardContext = () => useContext(DashboardContext);
+export default DashboardLayout;
