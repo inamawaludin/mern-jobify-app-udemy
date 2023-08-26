c013f611e03701ffb35002f0899dffa032f511fd
Muchammad In'am Awaludin <inam.awaludin@gmail.com>
2023-08-26 20:08:42 +0700

    create theme toggle functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,76 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import {
   HomeLayout,
   Register,
   Login,
   DashboardLayout,
   Error,
-  Landing
-} from './pages'
+  Landing,
+  AddJob,
+  Stats,
+  AllJobs,
+  Profile,
+  Admin,
+} from "./pages";
+
+export const checkDefaultTheme = () => {
+  const isDarkTheme = localStorage.getItem("darkTheme") === "true";
+  document.body.classList.toggle("dark-theme", isDarkTheme);
+  return isDarkTheme;
+};
+
+checkDefaultTheme();
+
 const router = createBrowserRouter([
   {
-    path : '/',
-    element : <HomeLayout />,
-    errorElement : <Error />,
-    children : [
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
       {
-        index : true,
-        element : <Landing />
+        index: true,
+        element: <Landing />,
       },
       {
-        path : 'register',
-        element : <Register />
+        path: "register",
+        element: <Register />,
       },
       {
-        path : 'login',
-        element : <Login />
+        path: "login",
+        element: <Login />,
       },
       {
-        path : 'dashboard',
-        element : <DashboardLayout />
-      }
-    ]
+        path: "dashboard",
+        element: <DashboardLayout />,
+        children: [
+          {
+            index: true,
+            element: <AddJob />,
+          },
+          {
+            path: "stats",
+            element: <Stats />,
+          },
+          {
+            path: "all-jobs",
+            element: <AllJobs />,
+          },
+          {
+            path: "profile",
+            element: <Profile />,
+          },
+          {
+            path: "admin",
+            element: <Admin />,
+          },
+        ],
+      },
+    ],
   },
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
