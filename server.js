ec1b2fb4b9d97cf9b6981ccecdba3b0e25a88477
Muchammad In'am Awaludin <inam.awaludin@gmail.com>
2023-08-27 18:08:17 +0700

    Create CRUD for job

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import morgan from "morgan";
+import * as dotenv from "dotenv";
+import jobRouter from "./routes/jobRouter.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+
+
+app.use("/api/v1/jobs", jobRouter);
+
+
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "something went wrong" });
+});
+
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
