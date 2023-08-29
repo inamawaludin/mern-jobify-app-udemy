85b0284ef3fe60de305978ae1c55cf3d62a58744
Muchammad In'am Awaludin <inam.awaludin@gmail.com>
2023-08-29 14:13:19 +0700

    Update jobController.js

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -16,7 +16,6 @@ export const createJob = async (req, res) => {
 export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
-  if (!job) throw new NotFoundError(`no job with id ${id}`);
   res.status(StatusCodes.OK).json({ job });
 };
 
@@ -25,7 +24,6 @@ export const updateJob = async (req, res) => {
   const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
     new: true,
   });
-  if (!updatedJob) throw new NotFoundError(`no job with id ${id}`);
   res.status(StatusCodes.OK).json({ msg: "job modified", job: updatedJob });
 };
 
@@ -33,7 +31,6 @@ export const deleteJob = async (req, res) => {
   const { id } = req.params;
 
   const removeJob = await Job.findByIdAndDelete(id);
-  if (!removeJob) throw new NotFoundError(`no job with id ${id}`);
 
   res.status(StatusCodes.OK).json({ msg: "job deleted", job: removeJob });
 };
