672e9efcce21214d73a45b3dcb0ee8c2c5eda1e3
Muchammad In'am Awaludin <inam.awaludin@gmail.com>
2023-09-05 16:24:53 +0700

    setup cloudinary

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ import userRouter from "./routes/userRouter.js";
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import cookieParser from "cookie-parser";
 import { authenticateUser } from "./middleware/authMiddleware.js";
+import cloudinary from "cloudinary";
+
+// public
+
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -17,14 +23,23 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use("/api/v1/users",authenticateUser, userRouter);
-app.use("/api/v1/jobs",authenticateUser, jobRouter);
-app.use("/api/v1/auth", authRouter);
+app.use(express.static(path.resolve(__dirname, "./public")));
 
+app.use("/api/v1/users", authenticateUser, userRouter);
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
