538f5a8da618111b661d18f375b7f964e09ba05c
Muchammad In'am Awaludin <inam.awaludin@gmail.com>
2023-09-08 13:45:29 +0700

    Setup Pagination

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,10 +1,11 @@
 import Job from "./Job";
 import Wrapper from "../assets/wrappers/JobsContainer";
 import { useAllJobsContext } from "../pages/AllJobs";
+import PageBtnContainer from "./PageBtnContainer";
 
 const JobsContainer = () => {
   const { data } = useAllJobsContext();
-  const { jobs } = data;
+  const { jobs, totalJobs, numOfPages } = data;
   console.log(jobs);
   if (jobs.length === 0) {
     return (
@@ -14,11 +15,13 @@ const JobsContainer = () => {
     );
   }
   return <Wrapper>
+    <h5>{totalJobs} job{jobs.length > 1 && 's'}</h5>
     <div className="jobs">
         {jobs.map((job)=>{
             return <Job key={job._id} {...job} />
         })}
     </div>
+    {numOfPages > 1 && <PageBtnContainer />}
   </Wrapper>;
 };
 export default JobsContainer;
