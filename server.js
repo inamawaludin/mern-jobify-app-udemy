1080097e17540e3c368ddd092baf10e9e850c332
Muchammad In'am Awaludin <inam.awaludin@gmail.com>
2023-09-09 14:53:07 +0700

    Change public path and implement secure middleware

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from "express";
 import morgan from "morgan";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
 
 import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
@@ -22,6 +24,8 @@ dotenv.config();
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
+app.use(helmet());
+app.use(mongoSanitize());
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -35,14 +39,14 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 app.use("*", (req, res) => {
